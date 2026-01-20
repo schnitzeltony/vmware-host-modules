53f17f3ce0592d843ce928d8f4211bc51feab685
Alexander A. Podlesnyy <a.podlesny@transtelematica.ru>
2026-01-20 08:25:44 +0300

    Some changes for kernel 6.18.5.

--- vmmon-only/linux/hostif.c
+++ vmmon-only/linux/hostif.c
@@ -2019,7 +2019,7 @@ HostIF_InitUptime(void)
 void
 HostIF_CleanupUptime(void)
 {
-   del_timer_sync(&uptimeState.timer);
+   timer_delete_sync(&uptimeState.timer);
 }
 
 
@@ -3429,7 +3429,7 @@ HostIF_SafeRDMSR(unsigned int msr,   // IN
    int err;
    u64 v;
 
-   err = rdmsrl_safe(msr, &v);
+   err = rdmsrq_safe(msr, &v);
    *val = (err == 0) ? v : 0;  // Linux corrupts 'v' on error
 
    return err;
